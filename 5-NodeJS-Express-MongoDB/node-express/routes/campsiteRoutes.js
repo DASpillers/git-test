d3d257b246e7716341f8c56449f1f213b81acc26
Daniel Spillers <spillersdaniela@gmail.com>
2021-11-10 09:48:09 -0500

    Express Generator

--- 5-NodeJS-Express-MongoDB/node-express/routes/campsiteRoutes.js
+++ 5-NodeJS-Express-MongoDB/node-express/routes/campsiteRoutes.js
@@ -24,4 +24,30 @@ campsiteRouter
     res.end("Deleting all campsites");
   });
 
+campsiteRouter
+  .route("/:campsiteId")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res) => {
+    res.end(`Will send campsites: ${req.params.campsiteId}`);
+  })
+  .post((req, res) => {
+    res.statusCode = 403;
+    res.end(
+      `POST operation not supported on /campsites/${req.params.campsiteId}`
+    );
+  })
+  .put((req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}`);
+    res.end(
+      `PUT operation not supported on /campsites/${req.params.campsiteId}`
+    );
+  })
+  .delete((req, res) => {
+    res.end("Deleting all campsites");
+  });
+
 module.exports = campsiteRouter;
