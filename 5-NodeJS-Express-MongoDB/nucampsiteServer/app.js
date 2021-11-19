34df5de484e5ba0f4f7b94a8c74919e3c590879f
Daniel Spillers <spillersdaniela@gmail.com>
2021-11-19 14:53:37 -0500

    Express Sesion Part 1

--- 5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
@@ -4,6 +4,8 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 
 const url = "mongodb://127.0.0.1:27017/nucampsite";
 const connect = mongoose.connect(url, {
@@ -33,11 +35,24 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("12345-67890-09876-54321"));
+
+// app.use(cookieParser("12345-67890-09876-54321"));
+
+app.use(
+  session({
+    name: "session-id",
+    secret: "12345-67890-09876-54321", //sign-in cookie
+    saveUninitialized: false, //new sessions with no updates won't get saved. wont send cookies.
+    resave: false, // when session is created, updated, and saved, it will continue to be resaved. keeps session marked as active
+    store: new FileStore(), //creates new filestore object to saved session information to server hard disk
+  })
+);
 
 //AUTHENTICATION: CUSTOM MIDDLEWARE AUTHENTICATION
 function auth(req, res, next) {
-  if (!req.signedCookies.user) {
+  console.log(req.session);
+
+  if (!req.session.user) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
       const err = new Error("You are not authenticated!");
@@ -52,7 +67,7 @@ function auth(req, res, next) {
     const user = auth[0];
     const pass = auth[1];
     if (user === "admin" && pass === "password") {
-      res.cookie("user", "admin", { signed: true });
+      res.session.user = 'admin';
       return next(); // authorized
     } else {
       const err = new Error("You are not authenticated!");
@@ -61,7 +76,7 @@ function auth(req, res, next) {
       return next(err);
     }
   } else {
-    if (req.signedCookies.user === "admin") {
+    if (req.session.user === "admin") {
       return next();
     } else {
       const err = new Error("You are not authenticated!");
